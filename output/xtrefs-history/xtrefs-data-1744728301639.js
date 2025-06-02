20177e316560bcfb457ad99f5a9b30127d5e3b26
henkvancann <hvancann@bcws.io>
2025-06-02 14:37:23 +0200

    updated spec-up-t to the latest
    Signed-off-by: henkvancann <hvancann@bcws.io>

---
+++ output/xtrefs-history/xtrefs-data-1744728301639.js
@@ -0,0 +1,71 @@
+const allXTrefs = {
+  "xtrefs": [
+    {
+      "externalSpec": "keri1",
+      "term": "AID",
+      "repoUrl": "https://github.com/weboftrust/kerisuite-glossary",
+      "terms_dir": "spec/terms-definitions",
+      "owner": "weboftrust",
+      "repo": "kerisuite-glossary",
+      "site": null,
+      "commitHash": "not found",
+      "content": "This term was not found in the external repository."
+    },
+    {
+      "externalSpec": "keri1",
+      "term": "control authority",
+      "repoUrl": "https://github.com/weboftrust/kerisuite-glossary",
+      "terms_dir": "spec/terms-definitions",
+      "owner": "weboftrust",
+      "repo": "kerisuite-glossary",
+      "site": null,
+      "commitHash": "not found",
+      "content": "This term was not found in the external repository."
+    },
+    {
+      "externalSpec": "toip1",
+      "term": "Identity Assurance",
+      "repoUrl": "https://github.com/henkvancann/ctwg-main-glossary",
+      "terms_dir": "spec/terms-definitions",
+      "owner": "henkvancann",
+      "repo": "ctwg-main-glossary",
+      "site": null,
+      "commitHash": "not found",
+      "content": "This term was not found in the external repository."
+    },
+    {
+      "externalSpec": "toip2",
+      "term": "Non-Disclosure Agreement",
+      "repoUrl": "https://github.com/henkvancann/ctwg-general-glossary",
+      "terms_dir": "spec/terms-definitions",
+      "owner": "henkvancann",
+      "repo": "ctwg-general-glossary",
+      "site": null,
+      "commitHash": "not found",
+      "content": "This term was not found in the external repository."
+    },
+    {
+      "externalSpec": "keri1",
+      "term": "out-of-band-introduction",
+      "repoUrl": "https://github.com/weboftrust/kerisuite-glossary",
+      "terms_dir": "spec/terms-definitions",
+      "owner": "weboftrust",
+      "repo": "kerisuite-glossary",
+      "avatarUrl": "https://avatars.githubusercontent.com/u/82824804?v=4",
+      "site": null,
+      "commitHash": "3d264daeef94cbed14dff45c5f03adaf1303aa44",
+      "content": "[[def: out-of-band-introduction, out-of-band introduction]]\n\n~ Out-of-band Introductions (OOBIs) are discovery and validation of IP resources for [[ref: key-event-receipt-infrastructure]] autonomic identifiers. **Discovery via URI, trust via KERI.**\n\n~ The simplest form of a KERI OOBI is a namespaced string, a tuple, a mapping, a structured message, or structured attachment that contains both a KERI AID and a URL. The OOBI associates the URL with the AID. \n\n~ More in <a href=\"https://weboftrust.github.io/WOT-terms/docs/glossary/out-of-band-introduction\">extended KERI glossary</a>\n"
+    },
+    {
+      "externalSpec": "toip2",
+      "term": "pdf",
+      "repoUrl": "https://github.com/henkvancann/ctwg-general-glossary",
+      "terms_dir": "spec/terms-definitions",
+      "owner": "henkvancann",
+      "repo": "ctwg-general-glossary",
+      "site": null,
+      "commitHash": "not found",
+      "content": "This term was not found in the external repository."
+    }
+  ]
+};
